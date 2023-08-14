6eb77848cc7f365fbca3a1f8233c61ddb13a8efd
magic990619 <magicrjh0619@gmail.com>
2023-08-14 07:59:50 -0400

    feat: add Patex network

--- src/addresses.ts
+++ src/addresses.ts
@@ -145,6 +145,16 @@ const BASE_GOERLI_ADDRESSES: ChainAddresses = {
   swapRouter02Address: '0x8357227D4eDc78991Db6FDB9bD6ADE250536dE1d'
 }
 
+const PATEX_ADDRESSES: ChainAddresses = {
+  v3CoreFactoryAddress: '0x33128a8fC17869897dcE68Ed026d694621f6FDfD',
+  multicallAddress: '0x091e99cb1C49331a94dD62755D168E941AbD0693',
+  quoterAddress: '0x3d4e44Eb1374240CE5F1B871ab261CD16335B76a',
+  v3MigratorAddress: '0x23cF10b1ee3AdfCA73B0eF17C07F7577e7ACd2d7',
+  nonfungiblePositionManagerAddress: '0x03a520b32C04BF3bEEf7BEb72E919cf822Ed34f1',
+  tickLensAddress: '0x0CdeE061c75D43c82520eD998C23ac2991c9ac6d',
+  swapRouter02Address: '0x2626664c2603336E57B271c5C0b26F421741e481'
+}
+
 export const CHAIN_TO_ADDRESSES_MAP: Record<SupportedChainsType, ChainAddresses> = {
   [ChainId.MAINNET]: MAINNET_ADDRESSES,
   [ChainId.OPTIMISM]: OPTIMISM_ADDRESSES,
@@ -160,7 +170,8 @@ export const CHAIN_TO_ADDRESSES_MAP: Record<SupportedChainsType, ChainAddresses>
   [ChainId.SEPOLIA]: SEPOLIA_ADDRESSES,
   [ChainId.AVALANCHE]: AVALANCHE_ADDRESSES,
   [ChainId.BASE]: BASE_ADDRESSES,
-  [ChainId.BASE_GOERLI]: BASE_GOERLI_ADDRESSES
+  [ChainId.BASE_GOERLI]: BASE_GOERLI_ADDRESSES,
+  [ChainId.PATEX]: PATEX_ADDRESSES
 }
 
 /* V3 Contract Addresses */
